62a6fdf3cd077a1d10a821af8cd2830f1293b108
dchappie <dchappie@student.wethinkcode.co.za>
2021-02-11 00:37:38 +0200

    edit aff_a

--- Projects/Exams/Beginner/level00/solutions/aff_a.c
+++ Projects/Exams/Beginner/level00/solutions/aff_a.c
@@ -6,7 +6,7 @@
 /*   By: dchappie <dchappie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 23:32:31 by dchappie          #+#    #+#             */
-/*   Updated: 2021/02/10 23:47:26 by dchappie         ###   ########.fr       */
+/*   Updated: 2021/02/11 00:36:08 by dchappie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,17 @@ void    aff_a(char *str)
     {
         if (str[index] == 'a')
         {
-            write(1, "a", 1);
+            write(1, "a\n", 2);
             return;
         }
     }
+    write(1, "\n", 1);
 }
 
 int     main(int argc, char **argv)
 {
     if (argc == 2)
-    {
         aff_a(argv[1]);
-        write(1, "\n", 1);
-    }
     else
         write(1, "a\n", 2);
     return (0);
