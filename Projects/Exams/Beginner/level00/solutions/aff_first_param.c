eb107f78e148346bc4c9464cf2ea3d5067c482ab
DamianOdendaal <dodendaa@student.wethinkcode.co.za>
2021-02-10 19:33:51 +0200

    added aff_first_param.c

---
+++ Projects/Exams/Beginner/level00/solutions/aff_first_param.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_first_param.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dodoTheHacker <dodendaa@student.wethink    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/10 18:20:27 by dodoTheHack       #+#    #+#             */
+/*   Updated: 2021/02/10 18:31:06 by dodoTheHack      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);   
+}
+
+
+void    ft_putstr(char *str)
+{
+    int index;
+
+    index = 0;
+    while(str[index])
+    {
+        ft_putchar(str[index]);
+        index++;
+    }
+}
+
+void    aff_first_param(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        ft_putchar('\n');
+        return ;
+    }
+    ft_putstr(argv[1]);
+    ft_putchar('\n');
+}
+
+int     main(int argc, char **argv)
+{   
+    aff_first_param(argc, argv);
+    return (0);
+}
